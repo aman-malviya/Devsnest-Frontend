630efea53dd157f02eb87888f8e5a8762cf95ded
Aman <amanmalviya14032000@gmail.com>
2021-06-07 21:03:02 +0530

    Devsnest Frontend HTML CSS Tasks completed

---
+++ Day4/clock.js
@@ -0,0 +1,18 @@
+const hour=document.getElementById("hour");
+const minute=document.getElementById("minute");
+const second=document.getElementById("second");
+
+setInterval(() => {
+    rotate();
+}, 1000);
+
+function rotate(){
+    let h=new Date().getHours() % 12;
+    let m=new Date().getMinutes();
+    let s=new Date().getSeconds();
+
+    hour.style.transform='rotate('+ (h/12)*360 +'deg)';
+    minute.style.transform='rotate('+ (m/60)*360 +'deg)';
+    second.style.transform='rotate('+ (s/60)*360 +'deg)';
+
+}
